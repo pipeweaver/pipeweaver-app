663dc3ae6718de9260b1e0ec212b3df0981e8ebd
Craig McLure <craig@mclure.net>
2026-03-16 02:05:48 +0000

    Set Qt 6.2.0 as a hard minimum

--- build.rs
+++ build.rs
@@ -11,7 +11,7 @@ fn main() {
         .expect("Parsing Qt version failed");
 
     // QTWebEngine isn't available before 6.2.0, so bail if that's not present
-    if qt_version >= Version::new(6, 0, 0) && qt_version < Version::new(6, 2, 0) {
+    if qt_version < Version::new(6, 2, 0) {
         panic!(
             "QT Web Engine not available on this QT Version: {}",
             qt_version
